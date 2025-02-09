e728ad749d937ed7fc47596074b4bee8b17c6446
Metin Mert Hot <metin.mert.hot@gmail.com>
2025-02-09 20:49:04 +0300

    Update apiSlice.js

--- frontend/src/store/api/apiSlice.js
+++ frontend/src/store/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://mern-ecommerce-backend-gb9t.onrender.com',
+    baseUrl: 'https://mern-ecommerce-backend-gb9t.onrender.com/api',
     credentials: 'include',
     prepareHeaders: (headers) => {
         headers.set('Content-Type', 'application/json');
